85313c2a8c2fc6990f5a692821ecad7ad0777ecf
Ashley <a_falsario@yahoo.com>
2015-03-20 15:15:54 -0500

    Adding action

--- lorem-generator/lorem-generator.php
+++ lorem-generator/lorem-generator.php
@@ -1,3 +1,13 @@
-<<?php 
+<?php
+
+/*
+Plugin Name: Lorem Generator
+Plugin URI: http://ashleyfalsario.com
+Description: Plugin that adds a button to the WYSIWYG editor which generates lorem text.
+Version: 1.0
+Author: Ashley Falsario
+Author URI: http://ashleyfalsario.com
+License: GPLv2 or later
+*/
 
  ?>
